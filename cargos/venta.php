279af1acac5a3025f04df999eed5c8b9f7e40b09
Erwin Arancibia <edarancibia11@gmail.com>
2016-06-20 17:14:13 -0400

    Interfaz adm pacientes

--- cargos/venta.php
+++ cargos/venta.php
@@ -8,7 +8,47 @@ $col = 'apellido';
 $result = $med->getAll($col);
 
 ?>
+<style type="text/css">
+		/*Panel tabs*/
+.panel-tabs {
+    position: relative;
+    bottom: 30px;
+    clear:both;
+    border-bottom: 1px solid transparent;
+}
 
+.panel-tabs > li {
+    float: left;
+    margin-bottom: -1px;
+}
+
+.panel-tabs > li > a {
+    margin-right: 2px;
+    margin-top: 4px;
+    line-height: .85;
+    border: 1px solid transparent;
+    border-radius: 4px 4px 0 0;
+    color: #ffffff;
+}
+
+.panel-tabs > li > a:hover {
+    border-color: transparent;
+    color: #ffffff;
+    background-color: transparent;
+}
+
+.panel-tabs > li.active > a,
+.panel-tabs > li.active > a:hover,
+.panel-tabs > li.active > a:focus {
+    color: #fff;
+    cursor: default;
+    -webkit-border-radius: 2px;
+    -moz-border-radius: 2px;
+    border-radius: 2px;
+    background-color: rgba(255,255,255, .23);
+    border-bottom-color: transparent;
+}
+</style>
 <div class="container">
              <div class="panel panel-primary">
                 <div class="panel-heading">
@@ -26,7 +66,87 @@ $result = $med->getAll($col);
                         <div class="tab-pane active" id="tab1">
                         	<h4>Admision de pacientes</h4>
 
-                        	<div></div>
+                        	<form id="form2" method="post">
+                        		<div class="panel panel-info">
+                        			<div class="panel-body">
+                        				<div class="form-group">
+                        					<div class="col-xs-2">
+                        						<input type="text" class="form-control" id="txtRutNum2" placeholder="Rut" maxlength="8"></input>
+                        					</div>
+                        					<div class="col-xs-1">
+                        						<input type="text" class="form-control" id="txtRutVer2" maxlength="1" disabled="true"></input>
+                        					</div>
+                        				</div>
+
+                        				<br><br>
+                        				<div class="form-group">
+								            
+								            <div class="col-xs-3">
+								                <input type="text" class="form-control" placeholder="Nombres" id="txtNomPac" />
+								            </div>
+								            <div class="col-xs-3">
+								                <input type="text" class="form-control" placeholder="A. Paterno" id="txtApat" />
+								            </div>
+								            <div class="col-xs-3">
+								                <input type="text" class="form-control" placeholder="A. Materno" id="txtAmat" />
+								            </div>
+								            <div class="col-xs-1">
+								            	<label>Fecha nac:</label>
+								            </div>
+								            <div class="col-xs-2">
+								            	<input type="date" class="form-control" id="txtFnac"></input>
+								            </div>
+								        </div>
+
+								        <br><br>
+
+								        <div class="form-group">
+								        	<div class="col-xs-1">
+								        		<input type="text" class="form-control" id="txtEdad" placeholder="Edad"></input>
+								        	</div>
+								        	<div class="col-xs-1">
+								        		<input type="radio" id="rdEdad" name="edad" value="1">Años</input>
+								        		<input type="radio" id="rdEdad" name="edad" value="2">Meses</input>
+								        		<input type="radio" id="rdEdad" name="edad" value="3">Dias</input>
+								        	</div>
+								        	<div class="col-xs-3">
+								        		<strong>Sexo:</strong><br>
+								        		<input type="radio" name="sexo" id="rdSexo" value="1">Masculino</input>
+								        		<input type="radio" name="sexo" id="rdSexo" value="0">Femenino</input>
+								        	</div>
+								        	<div class="col-xs-3">
+								        		<input type="text" class="form-control" placeholder="Dirección" id="txtDireccion"></input>
+								        	</div>
+								        	<div class="col-xs-3">
+								        		<input type="text" class="form-control" id="txtTelefono" placeholder="Teléfono"></input>
+								        	</div>
+								        </div>
+								        <br><br><br>
+								        <div class="form-group">
+								        	<div class="col-xs-3">
+								        		<strong>Cuidad</strong>
+								        		<select id="cboCiudad" class="form-control">
+								        			<option value="1">OSORNO</option>
+								        		    <option value="2">PUERTO MONTT</option>
+								        		    <option value="3">VALDIVIA</option>
+								        		</select>
+								        	</div><br>
+								        	<div class="col-xs-3">
+								        		<input type="text" id="txtActividad" class="form-control" placeholder="Actividad"></input>
+								        	</div>
+								        	<div class="col-xs-3">
+								        		<input type="text" id="txtEmail" class="form-control" placeholder="Email"></input>
+								        	</div>
+								        </div>
+								        <br><br>
+								        <div class="form-group">
+								        	<div class="col-xs-3">
+								        		<input type="submit" class="btn btn-primary" id="btnkPac" value="Guardar"></input>
+								        	</div>
+								        </div>
+								    </div>
+                        		</div>
+                        	</form>
 
                         </div>
                         <div class="tab-pane" id="tab2">
@@ -131,33 +251,7 @@ $result = $med->getAll($col);
 
 			    				<input type="submit" value="Ok" class="btn btn-primary input fuente_btn" id="btnOKserv"></input>
 			    				<label><h4>Servicios</h4></label>
-
-			    				<table class="table table-hover">
-			    					<thead>
-			    						<tr>
-			    							<td>Codigo</td>
-			    							<td>Prestacion</td>
-			    						</tr>
-			    					</thead>
-
-			    					<?php
-			    						if (!empty($servicio)) {
-			    							foreach ($servicio as $item) {
-			    					?>
-			    					<tr>
-			    						<td><?php echo $item["Codigo"]; ?></td>
-			    						<td><?php echo $item["Prestacion"]; ?></td>
-			    					</tr>	
-			    					<?php		
-			    							}
-			    					?>	
-			    				</table>	
-			    					<?php }else{
-					                    echo "<br>";
-					                    echo "No hay resultados";
-					                   }
-					                    
-					                ?>			
+		
 			    			</div>
 			    		</div>
 			    	</div>
