bcc1dd6be40749dc7ea2375c407620e00a55f8f6
cristianruiz <crisivex@gmail.com>
2016-08-05 15:38:46 -0400

    h1

--- controller/honorarios.php
+++ controller/honorarios.php
@@ -1,1 +1,22 @@
 <?php
+$action=$_GET["action"];
+switch ($action) {
+	case "honorariosmensual":
+		$mes= $_GET["mes"];
+		$ano= $_GET["ano"];
+		$params= array("ano"=>$ano,"mes"=>$mes);
+		print_r($params);
+		$client=new SoapClient('http://192.168.1.51:8080/cbows/admision?wsdl');
+		echo( $client->honorariosPad($params)->return);
+
+
+		break;
+
+	default:
+		break;
+}
+
+
+
+
+?>
