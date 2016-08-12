ebb50f68c404143160087604a326dee974b784b3
cristianruiz <crisivex@gmail.com>
2016-08-12 15:15:12 -0400

    h

--- newfile.php
+++ newfile.php
@@ -7,7 +7,7 @@ $srv= new Serv();
 print_r($srv->json_buscador_servicios());
 */
 include('drivers/hm_honorariosicbo.php');
-$h= new hm_honorariosicbo(6, 2016);
+$h= new hm_honorariosicbo('JUNIO', 2016);
 print_r($h->get_periodo());
 
 
