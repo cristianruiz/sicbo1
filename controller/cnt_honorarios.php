518da51ae4fdd3f35f812a376f57762e81388134
cristianruiz <crisivex@gmail.com>
2016-08-16 23:33:37 -0400

    f

--- controller/cnt_honorarios.php
+++ controller/cnt_honorarios.php
@@ -9,7 +9,7 @@ class hm_honorarioconsolidado extends HmHonorarioconsolidadoMySqlDAO{
 	public function cargahonorarioconsolidaddo(){
 		$sql="insert hm_honorarioconsolidado(rutmed,formula,valor,idhonorario)
 			select t1.rutmed as rutmed ,t2.formula as formula,sum(t1.monto) as valor,
-			".$this->idhonorariosicbo." as idhonorario,1 as tiporeceptor
+			".$this->idhonorariosicbo." as idhonorario 
 			from hm_detallehonorariossicbo t1,hm_homocodigosformulas t2
 			where t1.codigo=t2.codigoservicio and t1.periodo=".$this->periodo."
 			
