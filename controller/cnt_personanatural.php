d95a6cb0c66a29fea614926ae1829a20212d5833
cristianruiz <crisivex@gmail.com>
2016-08-25 08:10:58 -0400

    f

--- controller/cnt_personanatural.php
+++ controller/cnt_personanatural.php
@@ -1,14 +1,15 @@
 <?php 
 class cnt_personanatural extends HmPersonanaturalMySqlDAO{
 	function carganombredesdesicbo($r){
-		$sql="select *
+		$sql="select medico
 		from hm_detallehonorariossicbo
 			where rutmed='".$r."' 
 			order by iddetallehonorario desc limit 0,1";
+		print_r($sql."<br>");
 		$sqlQuery = new SqlQuery($sql);
 		$arr=$this->execute($sqlQuery);
+		print_r("MED: ".$arr->medico."<br>");
 		
-		print_r($arr["medico"]);
 		return($arr["medico"]);
 	}
 }
