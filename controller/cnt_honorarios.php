d30d5a7e35d05c66d33bb1701a6df059a34dbcad
cristianruiz <crisivex@gmail.com>
2016-08-30 15:25:57 -0400

    f

--- controller/cnt_honorarios.php
+++ controller/cnt_honorarios.php
@@ -58,13 +58,22 @@ class hm_honorarioconsolidado extends HmHonorarioconsolidadoMySqlDAO{
 		return(json_encode($ret));
 	}
 	public function cargahonorarioconsolidaddo(){
-		$sql="insert hm_honorarioconsolidado(rutmed,formula,valor,idhonorariosicbo)
+		/*$sql="insert hm_honorarioconsolidado(rutmed,formula,valor,idhonorariosicbo)
 			select t1.rutmed as rutmed ,t2.formula as formula,sum(t1.monto) as valor,
 			".$this->idhonorariosicbo." as idhonorariosicbo 
 			from hm_detallehonorariossicbo t1,hm_homocodigosformulas t2
 			where t1.codigo=t2.codigoservicio and t1.periodo=".$this->periodo."
 			
-			group by t1.rutmed,t2.formula";
+			group by t1.rutmed,t2.formula";*/
+		$sql="select t1.rutmed as rutmed ,t2.formula as formula,sum(t1.monto) as valor,
+			".$this->idhonorariosicbo." as idhonorariosicbo ,
+			ifnull(t3.esreceptor,1) as receptor
+			from hm_detallehonorariossicbo t1 inner join hm_homocodigosformulas t2 on t1.codigo=t2.codigoservicio
+            left  join hm_personanatural t3 on t1.rutmed=t3.rutproveedor
+			where   t1.periodo=".$this->periodo." 
+			and t1.rutmed<>81949100
+			
+			group by t1.rutmed,t2.formula,t3.esreceptor";
 		$sqlQuery = new SqlQuery($sql);
 		$numrows=$this->executeInsert($sqlQuery);
 		return $num_rows;
