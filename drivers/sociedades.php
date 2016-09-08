625181f46b0d5aa8f89deca2b2df4670e4b64390
cristianruiz <crisivex@gmail.com>
2016-09-08 17:34:25 -0300

    gh

--- drivers/sociedades.php
+++ drivers/sociedades.php
@@ -7,7 +7,8 @@ class sociedad extends HmSociedadMySqlDAO{
 		$rer=new HmSociedad();
 		
 		$rer=$this->queryByRutsociedad($rut);
-		error_log("rerrrrr:   ".print_r($rer),true);
+		error_log("rerrrr");
+		error_log(print_r($rer,true));
 		return $rer->razonsocial;
 	}
 	
